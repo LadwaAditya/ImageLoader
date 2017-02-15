2fcf6b2b500e787ebb3931690f6cdea38e521dee
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-15 15:09:05 +0530

    Fect(FacebookSignin): Added Helper for Facebook signin

---
+++ app/src/main/java/com/ladwa/aditya/image/ui/chooseimage/facebookSignIn/FacebookResponse.java
@@ -0,0 +1,14 @@
+package com.ladwa.aditya.image.ui.chooseimage.facebookSignIn;
+
+/**
+ * Created by multidots on 6/16/2016.
+ */
+public interface FacebookResponse {
+    void onFbSignInFail();
+
+    void onFbSignInSuccess();
+
+    void onFbProfileReceived(FacebookUser facebookUser);
+
+    void onFBSignOut();
+}
